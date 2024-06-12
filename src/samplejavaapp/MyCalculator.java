a42fa25e390c3ed15dc877aa35c8b6eb9f5011a9
Jomari S. Abejo <jomariabejostorage@gmail.com>
2024-06-12 08:49:16 +0800

    add close method, comments, update argument variable name and include arithmetic exception for division function

--- src/samplejavaapp/MyCalculator.java
+++ src/samplejavaapp/MyCalculator.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package samplejavaapp;
 
 /**
@@ -9,25 +5,60 @@ package samplejavaapp;
  * @author Armando III
  */
 public class MyCalculator {
-    
-    // Addition
-    public int AddFunction(int a, int b){
-        return a + b;
-    }
-    
-    // Subtraction
-    public int SubtractFunction(int a, int b){
-        return a - b;
-    }
-    
-    // Multiplication
-    public int MultiplyFunction(int a, int b){
-        return a * b;
-    }
-    
-    // Division
-    public int DivideFunction(int a, int b){
-        return a / b;
+    private final int ZERO = 0;
+    /**
+     * Adds two integers.
+     *
+     * @param primaryAddend The first addend.
+     * @param secondaryAddend The second addend.
+     * @return The sum of the two integers.
+     */
+    public int AddFunction(int primaryAddend, int secondaryAddend){
+        return primaryAddend + secondaryAddend;
     }
 
+    /**
+     * Subtracts one integer from another.
+     *
+     * @param minuend The number from which to subtract.
+     * @param subtrahend The number to subtract.
+     * @return The difference between the two integers.
+     */
+    public int SubtractFunction(int minuend, int subtrahend){
+        return minuend - subtrahend;
+    }
+
+    /**
+     * Multiplies two integers.
+     *
+     * @param multiplicand The first number to multiply.
+     * @param multiplier The second number to multiply.
+     * @return The product of the two integers.
+     */
+    public int MultiplyFunction(int multiplicand, int multiplier){
+        return multiplicand * multiplier;
+    }
+
+    /**
+     * Divides one integer by another.
+     *
+     * @param dividend The number to be divided.
+     * @param divisor The number by which to divide.
+     * @return The quotient of the division operation.
+     * @throws ArithmeticException If the divisor is zero.
+     */
+    public int DivideFunction(int dividend, int divisor){
+        if (divisor == ZERO) {
+            throw new ArithmeticException("Cannot divide by " + ZERO);
+        }
+        return dividend / divisor;
+    }
+
+    /**
+     * Closes the entire class and makes it eligible for garbage collection.
+     */
+    public void close() {
+        // Perform any necessary cleanup operations here
+        // Remove any references to this class instance
+    }
 }
